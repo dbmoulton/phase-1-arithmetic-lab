60dce5f922e532b2b3443248f8dac95e30fbeba2
David Moulton <dbmoulton@gmail.com>
2023-02-22 19:28:08 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 22;
+const num4 = 9;
 
-// Write your code here
+const multiply = num1 * num2 
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4
+const max = Math.max(1, 5, 6, 20);
